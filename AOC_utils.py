da8ec15a648112aef0e4a18390888b2cf9816a03
rycan88 <rycan888@gmail.com>
2024-12-05 19:57:48 -0800

    Day 5

---
+++ AOC_utils.py
@@ -0,0 +1,7 @@
+
+def getAroundList():
+    Around = [(0, 1), (1, 0), (1, 1), (-1, -1), (-1, 0), (0, -1), (-1, 1), (1, -1)]
+    return Around
+
+def addTups(t1, t2):
+    return (t1[0] + t2[0], t1[1] + t2[1])
